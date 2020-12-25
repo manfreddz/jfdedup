02fca9ec9c2931b97f588f5ff29b50e183f0727f
Manfred Dellkrantz <manfred.dellkrantz@verisure.com>
2020-12-25 17:59:54 +0100

    Switched to single built jar instead

--- compare.sh
+++ compare.sh
@@ -11,15 +11,17 @@ function title {
   echo "################################"
 }
 
+title "Building jfdedup..."
+mvn package > /dev/null 2> /dev/null
+
 title "Benchmarking fdupes..."
 sync; echo 3 |sudo tee /proc/sys/vm/drop_caches ;\
-time fdupes -rm "$dir"
+time fdupes -rm "$dir" 2> /dev/null > /dev/null
 
-title "Benchmarking jfdedup (mvn exec)..."
+title "Benchmarking jfdedup..."
 sync; echo 3 |sudo tee /proc/sys/vm/drop_caches ;\
-mvn compile ;\
-time mvn exec:java -Dexec.args="$dir"
+time java -jar target/jfdedup-1.0-SNAPSHOT-jar-with-dependencies.jar "$dir" > /dev/null
 
 title "Benchmarking rdfind..."
 sync; echo 3 |sudo tee /proc/sys/vm/drop_caches ;\
-time rdfind "$dir"
+time rdfind "$dir" > /dev/null
